a4d7eabe88aa542b4c6dc8cff51672bee9bd9ecb
EsPiRi <sezginsami@gmail.com>
2020-09-10 18:06:39 +0300

    Strings

---
+++ Strings/Strings.cpp
@@ -0,0 +1,17 @@
+﻿// Strings.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+// Author : Sami Sezgin
+
+#include <iostream>
+#define endl "\n"
+
+int main(){
+  std::string a, b;
+  char temp;
+  std::cin >> a >> b;
+  std::cout << a . size() << " " << b . size() << endl;
+  std::cout << a + b << endl;
+  temp = a[0];
+  a[0] = b[0];
+  b[0] = temp;
+  std::cout << a << " " << b << endl;
+}
